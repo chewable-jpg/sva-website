2f4f3a6ad951336d04fe25ffd409838eb02d2010
0x2366 <68360362+0x2366@users.noreply.github.com>
2021-11-18 15:17:10 +0100

    logo on hover test

--- src/components/UI/logo.jsx
+++ src/components/UI/logo.jsx
@@ -1,10 +1,12 @@
 import { chakra } from "@chakra-ui/react"
 import NextLink from "next/link"
 
+import styles from "./styles/logo.css"
+
 const Logo = () => (
   <>
     <NextLink href="/" passHref>
-      <chakra.a fontSize="2rem" fontWeight="700" color="#FE1919">
+      <chakra.a classname="logo" fontSize="2rem" fontWeight="700">
         SVA
       </chakra.a>
     </NextLink>
