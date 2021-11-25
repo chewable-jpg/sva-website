5630e6683c59f585f61cee75bec521d663a8888a
0x2366 <68360362+0x2366@users.noreply.github.com>
2021-11-25 09:55:50 +0100

    ifame postition test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -111,6 +111,13 @@ const Home = () => {
           </Box>
         ))}
       </Box>
+
+      <Box
+        alignItems={{ lg: "center"}}
+        justifyContent={{ lg: "center"}}
+        key={index}
+
+      >
       <iframe
               src="https://maps.google.com/maps?q=51.896013,%207.422888&t=k&z=15&ie=UTF8&iwloc=&output=embed"
               width="600"
@@ -121,6 +128,7 @@ const Home = () => {
               aria-hidden="false"
               tabIndex="0"
             />
+      </Box>
     </>
   )
 }
