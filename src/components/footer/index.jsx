7baaf5b13959b1426efb23e756c06f7954336916
0x2366 <68360362+0x2366@users.noreply.github.com>
2021-11-26 10:22:10 +0100

    nah bro

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -19,10 +19,12 @@ const Footer = () => (
       >
         <Box py="10"></Box>
         <Box py="2">
+          {/* <NextLink href="/contact">
             <Text>
               Öffnungszeiten: Dienstag: 17:00 - 18:00 Uhr | Mittwoch: 17:00 -
               18:00 Uhr
             </Text>
+          </NextLink> */}
         </Box>
       </Box>
     </Box>
